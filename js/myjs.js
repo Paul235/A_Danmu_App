a814a76a59874e8e0b81b7fa2373be23636cb573
Paul235 <1258035736@qq.com>
2016-12-23 15:51:21 +0800

    v1.0

---
+++ js/myjs.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    var colorList = ["AliceBlue", "AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "Darkorange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed", "Indigo", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow", "YellowGreen",];
+
+    $(".send").click(function() {
+        var str = $(".text").val();
+        var html = "";
+        //alert(str);
+        if (str.length == 0) {
+            alert("Please Input Something!");
+        }
+        html += "<p class='pull-right'>" + str + "</p>";
+        $(".content").append(html);
+
+        /*随机高度*/
+        var topNum = Math.floor(Math.random() * 470);
+        //alert(topNum);
+        $(".content p:last").css("top", topNum + "px");
+
+        /*随机颜色*/
+        var colorNum = Math.floor(Math.random()*(146-1)+1);
+        //alert(colorNum);
+        var colorName = colorList[colorNum];
+        //alert(colorName);
+        $(".content p:last").css("color", colorName);
+
+        /*清空输入框*/
+        $(".text").val("");
+
+    });
+
+    /*清屏*/
+    $(".clear").click(function() {
+        $(".content").html("");
+    });
+})
